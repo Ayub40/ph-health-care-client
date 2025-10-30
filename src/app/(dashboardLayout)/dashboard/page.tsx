3791dc446a69b26e896b7fcdd7cbd61e3b1c92aa
Ayub40 <ayubk4028@gmail.com>
2025-10-30 22:46:26 +0600

    Dashboard Component added

--- src/app/(dashboardLayout)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/dashboard/page.tsx
@@ -1,10 +1,10 @@
-
-
+import DashboardComponent from "@/components/modules/Dashboard/Dashboard";
 
 const Dashboard = () => {
     return (
         <div className="flex justify-center">
             <h1>Welcome to the Dashboard</h1>
+            <DashboardComponent />
         </div>
     );
 };
